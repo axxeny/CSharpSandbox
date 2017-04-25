55ff9a927d553c75155e53b1b969e1f595dcf079
Arseny Perchik at IK <login@axxeny.com>
2017-04-25 12:18:03 +0500

    Test serialization of IDictionary<string, object>.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Sandbox
 {
+    public class Obj
+    {
+        public Obj(IDictionary<string, object> objs)
+        {
+            Objs = objs;
+        }
+
+        public IDictionary<string, object> Objs { get; private set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(JsonConvert.SerializeObject(new Obj(new Dictionary<string, object>{["Abc"]=true})));
+            Console.ReadLine();
         }
     }
 }
